f909540aaff3d4c2737a735a3baadf828d57430a
Peter Schulam <pschulam@gmail.com>
2015-02-19 13:08:40 -0500

    Again...

--- subtype_model.py
+++ subtype_model.py
@@ -16,6 +16,7 @@ from .util import ConditionalPredictor
 
 Trajectory = namedtuple('Trajectory', ['key', 't', 'y', 'covariates'])
 
+
 class ConditionalPredictor:
     def __init__(self, model, trajectory):
         self.model = model
