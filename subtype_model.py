123c8efbd8ac54d655079285fbf90c3d3fb1944f
Peter Schulam <pschulam@gmail.com>
2015-02-27 00:55:39 -0500

    Include B-spline penalty during learning.

--- subtype_model.py
+++ subtype_model.py
@@ -15,11 +15,12 @@ from .util import ConditionalPredictor, Trajectory
 
 
 class SubtypeMixture:
-    def __init__(self, nsubtypes, ncovariates, basis_fn, cov_fn):
+    def __init__(self, nsubtypes, ncovariates, basis_fn, cov_fn, reg=None):
         self.nsubtypes = nsubtypes
         self.ncovariates = ncovariates
         self.basis = basis_fn
         self.cov = cov_fn
+        self.reg = reg
         self._init_params()
 
     def __copy__(self):
@@ -43,6 +44,12 @@ class SubtypeMixture:
         lpy = stats.multivariate_normal.logpdf(y, m, C)
 
         return lpz + lpy
+
+    def obs_loglik(self, trajectory, X=None, C=None):
+        X = self.basis(trajectory.t) if X is None else X
+        C = self.cov(trajectory.t) if C is None else C
+        ll = [self.loglik(trajectory, z, X, C) for z in range(self.nsubtypes)]
+        return logsumexp(ll)
 
     def posterior(self, trajectory):
         all_loglik = [self.loglik(trajectory, z) for z in range(self.nsubtypes)]
@@ -72,7 +79,7 @@ class SubtypeMixture:
     TrajectoryData = namedtuple(
         'TrajectoryData', ['X', 'C', 'cov_xx', 'cov_xy'])
 
-    def fit(self, trajectories, max_iter=100, tol=1e-5):
+    def fit(self, trajectories, devset=None, max_iter=100, tol=1e-5):
 
         ## Cache computations needed for estimation and store
         ## covariates of all trajectories in a single feature matrix.
@@ -94,6 +101,13 @@ class SubtypeMixture:
         covariates = np.vstack(covariates)
         qz = stats.dirichlet.rvs(self.nsubtypes * [0.1], len(trajectories))
 
+        dev_cache = OrderedDict()
+
+        for trj in devset:
+            X = self.basis(trj.t)
+            C = self.cov(trj.t)
+            dev_cache[trj.key] = self.TrajectoryData(X, C, None, None)
+
         ## EM iterations start here.
 
         iteration = 0
@@ -111,6 +125,9 @@ class SubtypeMixture:
 
             cov_xx = np.zeros((self.nsubtypes, self.basis.df, self.basis.df))
             cov_xy = np.zeros((self.nsubtypes, self.basis.df))
+
+            if self.reg is not None:
+                cov_xx += self.reg * self.basis.penalty
 
             for i, trj in enumerate(trajectories):
                 for z, w in enumerate(qz[i]):
@@ -134,11 +151,59 @@ class SubtypeMixture:
             delta = logl[iteration] - logl[iteration - 1]
             abs_delta = delta / abs(logl[iteration])
 
-            msg = 'Iter={:03d}, LL={:.02f}, Convergence={:.06f}'
-            msg = msg.format(iteration, logl[iteration], abs_delta)
+            if devset is not None:
+                dev_ll = 0.0
+                
+                for trj in devset:
+                    X = dev_cache[trj.key].X
+                    C = dev_cache[trj.key].C
+                    dev_ll += self.obs_loglik(trj, X, C)
+
+            else:
+                dev_ll = -float('inf')
+
+            msg = 'Iter={:03d}, LL={:.02f}, DevLL={:.02f}, Convergence={:.06f}'
+            msg = msg.format(iteration, logl[iteration], dev_ll, abs_delta)
             logging.info(msg)
 
             if iteration >= max_iter or abs_delta < tol:
                 break
 
         return self
+
+    def plot(self):
+        fig, ax = plt.subplots()
+        x = np.linspace(self.basis.lower, self.basis.upper, 100)
+        X = self.basis(x)
+        y = X.dot(self.coef.T)
+        ax.plot(x, y)
+        return fig
+
+    def plot_trajectories(self, trajectories=None):
+        ncol = int(np.ceil(np.sqrt(4.0 / 3.0 * self.nsubtypes)))
+        nrow = int(np.ceil(self.nsubtypes / float(ncol)))
+        fig, axes = plt.subplots(nrow, ncol)
+        x = np.linspace(self.basis.lower, self.basis.upper, 100)
+        X = self.basis(x)
+        y = X.dot(self.coef.T)
+
+        if trajectories is not None:
+            classified = [[] for _ in range(self.nsubtypes)]
+            
+            for trj in trajectories:
+                qz = self.posterior(trj)
+                z = np.argmax(qz)
+                classified[z].append(trj)
+
+        for i in range(self.nsubtypes):
+            ax = axes[np.unravel_index(i, (nrow, ncol))]
+            ax.plot(x, y[:, i])
+
+            if trajectories is not None:
+                for trj in classified[i]:
+                    ax.plot(trj.t, trj.y, '-k')
+
+            ax.set_ylim(0, 120)
+
+        return fig
+
